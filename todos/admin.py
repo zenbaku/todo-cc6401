e477d90e91976ffba7d51b415331f40da28aaa0e
Jonathan Urzua <zenbaku@gmail.com>
2016-08-07 23:00:37 -0400

    Crea modelo Todo con tests e integración en admin.

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'is_completed', 'created_at')
+    list_display_links = ('description',)
+    date_hierarchy = 'created_at'
+
+admin.site.register(Todo, TodoAdmin)
